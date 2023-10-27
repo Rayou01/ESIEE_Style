455f7a91f33ac653010fc6acd262bbc090dc43bd
ryanvernel <ryan.vernel@edu.esiee.fr>
2023-10-27 02:45:23 +0200

    Début d'insertion de connexion entre UI et code JAVA

---
+++ app/src/main/java/com/example/esieestyle/ConnectionFragment.java
@@ -0,0 +1,89 @@
+package com.example.esieestyle;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.esieestyle.databinding.FragmentConnectionBinding;
+
+public class ConnectionFragment extends Fragment {
+
+    private FragmentConnectionBinding binding;
+
+    public boolean is_User_Text_Empty, is_Password_Text_Empty;
+
+    public static ConnectionFragment newInstance() {
+        ConnectionFragment fragment = new ConnectionFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentConnectionBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //When the UI is created, we disable the connection button until the user enter something
+        binding.connectionButton.setEnabled(false);
+
+        binding.userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //We look if the user has enter a text for the user name EditText
+                is_User_Text_Empty = editable.toString().isEmpty();
+            }
+        });
+
+        binding.userPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //We look if the user has enter a text for the user password EditText
+                is_Password_Text_Empty = editable.toString().isEmpty();
+                //Then, we tes if both of EditText has been modified and enable the button if it's the case
+                binding.connectionButton.setEnabled(!(is_Password_Text_Empty&&is_User_Text_Empty));
+            }
+        });
+
+        binding.connectionButton.setOnClickListener(view1 -> {
+            //Naviguer vers l'activité suivante
+        });
+    }
+}
--- app/src/main/java/com/example/esieestyle/MainActivity.java
+++ app/src/main/java/com/example/esieestyle/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.esieestyle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+/*
+Reference pour l'avancer effectue par Ryan :
+https://openclassrooms.com/fr/courses/8150246-developpez-votre-premiere-application-android/8256305-accedez-aux-elements-interactifs-depuis-le-code-java-de-votre-fragment
+ */
 
 //Commit à chaque ajout de fonctionnalité ou réparation de bug
 
