0ca56e260055ce950a88884118b5166c7f6c53ca
ryanvernel <ryan.vernel@edu.esiee.fr>
2023-10-30 16:41:41 +0100

    Ajout de la 2ème activité et des images pour la toolbar

---
+++ app/README.md
@@ -0,0 +1,5 @@
+Nom de l'application : ESIEE Style
+
+Tâches : 
+
+-Mettre tout au long du développement une architecture MVVM
--- app/src/main/java/com/example/esieestyle/MainActivity.java
+++ app/src/main/java/com/example/esieestyle/MainActivity.java
@@ -1,20 +1,8 @@
 package com.example.esieestyle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
-import android.widget.Button;
 
-import com.example.esieestyle.databinding.FragmentConnectionBinding;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.Firebase;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
 /*
 Reference pour l'avancer effectue par Ryan :
 https://openclassrooms.com/fr/courses/8150246-developpez-votre-premiere-application-android/8256305-accedez-aux-elements-interactifs-depuis-le-code-java-de-votre-fragment
@@ -29,10 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
-        return super.onCreateView(name, context, attrs);
-    }
 }
+
+    //qui montre comment avec Firestore on peut lister, filtrer et noter en ligne des restaurants présentés dans une magnifique RecyclerView.
